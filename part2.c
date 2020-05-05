7515baa6ebd8adfd3adc7f76272ede38ca6b21a6
bhryck <randall2000n@gmail.com>
2020-05-05 15:03:34 +0700

    Add files via upload

---
+++ part2.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+char *FCFS(char *order_id[], int order_due[], int order_quant[], char *order_name, int plant_prod,)
+{
+    char resultstack[];
+
+    return resultstack;
+
+}
+void *FCFS(char *order_id[], int order_due[], int order_quant[], char *order_name, int plant_prod)
+{
+    char *resultstack[];
+
+    return resultstack;
+    
+}
+void *FCFS(char *order_id[], int order_due[], int order_quant[], char *order_name, int plant_prod)
+{
+    char resultstack[];
+
+    return resultstack;
+    
+}
+
+int main(){
+
+    //start period
+    int day;
+    int mon;
+    int year;
+
+    //end period
+    int day1;
+    int mon1;
+    int year1;
+
+    //order info
+    char *orderid[];    //order number
+    int orderdue[];     //due date
+    int orderquant[];   //Quantity
+    char *ordername[];  //Product name
+
+    //plant parameters
+    int plant_x = 300;
+    int plant_y = 400;
+    int plant_z = 500;
+
+    // edit these using function and send to part 3
+
+    int orderstack[];   //production orders in stack, lower index first , array[n] = order id, array[n+1] = order start, array[n+2] = order end
+    
+
+    //FCFS, SJF, PR
+    //get alg from outside
+    char *alg;
+
+    if(strcmp(alg,"FCFS")==0)
+    {
+        
+    }
+    else if(strcmp(alg,"SJF")==0)
+    {
+
+    }
+    else if(strcmp(alg,"PR")==0)
+    {
+        
+    }
+
+
+    return 0;
+}
